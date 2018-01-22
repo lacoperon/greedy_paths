8e8a566dffad4f92a40e9fad0c58b03b69468ec1
Elliot <elliot.james.williams@gmail.com>
2018-01-21 23:05:34 -0500

    Added basic unittest skeleton

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_basic(self): #Note that test methods must start with `test`
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
