8c8f73b01164b66c7e24e2c41ed3a1c6c1d93eaa
Elliot <elliot.james.williams@gmail.com>
2018-02-02 01:20:51 -0500

    Removed supirous test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-
-    def test_basic(self): #Note that test methods must start with `test`
-        self.assertTrue(True)
-
-if __name__ == '__main__':
-    unittest.main()
