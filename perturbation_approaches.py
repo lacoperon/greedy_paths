31f942e7b333501dd61954b1f9328c37e4631821
Elliot <elliot.james.williams@gmail.com>
2018-03-20 13:44:05 -0400

    Updated perturbation code to conditionally import networkit if not testin on Travis

--- perturbation_approaches.py
+++ perturbation_approaches.py
@@ -7,7 +7,9 @@ Effect of graph perturbation on routing @ various levels of 'greed'
 import networkx as nx
 import math
 import random
-# import networkit as nk
+import os
+if not 'TRAVIS' in os.environ or not os.environ['TRAVIS']:
+    import networkit as nk
 from functools import reduce
 import operator
 
